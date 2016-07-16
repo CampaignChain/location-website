1f3431ec0b0aa83da18e5c22af40c230ec83cbf6
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:21 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainLocationWebsiteBundle.php
+++ CampaignChainLocationWebsiteBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Location\WebsiteBundle;
 
+use CampaignChain\Location\WebsiteBundle\DependencyInjection\CampaignChainLocationWebsiteExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainLocationWebsiteBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainLocationWebsiteExtension();
+    }
 }
--- DependencyInjection/CampaignChainLocationWebsiteExtension.php
+++ DependencyInjection/CampaignChainLocationWebsiteExtension.php
@@ -2,9 +2,9 @@
 
 namespace CampaignChain\Location\WebsiteBundle\DependencyInjection;
 
+use CampaignChain\CoreBundle\Bundle\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -24,5 +24,7 @@ class CampaignChainLocationWebsiteExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->setParameters($container, $config);
     }
 }
